069c3dece4dfd0b86132caa9f23b59b6e96f6814
Will <changwei.cn@gmail.com>
2015-08-18 11:41:01 +0800

    建立dev分支

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 mvn install -DskipTests=true
 
-./saiku-server/target/dist/saiku-server/stop-saiku.sh
+kill -9  `lsof -i:8080 | awk '{print $2}'`
 ./saiku-server/target/dist/saiku-server/start-saiku.sh
 tail -f ./saiku-server/target/dist/saiku-server/tomcat/logs/saiku.log &
 tail -f ./saiku-server/target/dist/saiku-server/tomcat/logs/catalina.out&
