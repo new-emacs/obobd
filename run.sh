e25a1150cfb2afaa03f6d1f16ce87789524c4b6d
Will <changwei.cn@gmail.com>
2015-08-18 10:20:45 +0800

    合并代码后无法编译成功

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 mvn install -DskipTests=true
 
-./saiku-server/target/dist/saiku-server/start-saiku.sh
 ./saiku-server/target/dist/saiku-server/stop-saiku.sh
+./saiku-server/target/dist/saiku-server/start-saiku.sh
 tail -f ./saiku-server/target/dist/saiku-server/tomcat/logs/saiku.log &
 tail -f ./saiku-server/target/dist/saiku-server/tomcat/logs/catalina.out&
