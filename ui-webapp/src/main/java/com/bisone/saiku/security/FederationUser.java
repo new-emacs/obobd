db22f09768e23496ac67e7edd52a710fe6fa0a22
Will <changwei.cn@gmail.com>
2015-09-02 10:36:05 +0800

    sso不通过

--- ui-webapp/src/main/java/com/bisone/saiku/security/FederationUser.java
+++ ui-webapp/src/main/java/com/bisone/saiku/security/FederationUser.java
@@ -1,7 +1,28 @@
 package com.bisone.saiku.security;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
 /**
  * Created by will on 15-8-31.
  */
-public class FederationUser {
+public class FederationUser extends org.springframework.security.core.userdetails.User {
+
+    private static final long serialVersionUID = -2231762973730849416L;
+
+
+
+    public FederationUser(String username, String password, boolean enabled, boolean accountNonExpired,
+                          boolean credentialsNonExpired, boolean accountNonLocked,
+                          Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+
+    public FederationUser(String username, String password,
+                          Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, true, true, true, true, authorities);
+    }
+
+
 }
--- ui-webapp/src/main/java/com/bisone/saiku/security/SoneAuthenticationUserDetailsService.java
+++ ui-webapp/src/main/java/com/bisone/saiku/security/SoneAuthenticationUserDetailsService.java
@@ -1,7 +1,29 @@
 package com.bisone.saiku.security;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by will on 15-8-31.
  */
-public class SoneAuthenticationUserDetailsService {
+public class SoneAuthenticationUserDetailsService implements
+        AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
+
+
+    @Override
+    public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
+
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new FederationUser("admin", "admin", authorities);
+    }
 }
